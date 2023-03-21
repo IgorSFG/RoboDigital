b030c3f98aa9794295a4e138a3977a7bd4b0c873
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-03-21 15:48:32 -0300

    Simulação
    100% Funcionando!

--- Robo/HUD.gd
+++ Robo/HUD.gd
@@ -4,11 +4,6 @@ onready var posx = $GridContainer/PosXValue
 onready var posy = $GridContainer/PosYValue
 onready var posz = $GridContainer/PosZValue
 
-func _updatePositions(codx, cody, codz):
-	posx.text = codx
-	posy.text = cody
-	posz.text = codz
-
 func _on_Player_playerpos(vx, vy, vz):
 	posx.text = str(vx)
 	posy.text = str(vy)
--- Robo/Player.gd
+++ Robo/Player.gd
@@ -1,37 +1,46 @@
 extends KinematicBody
 
 const speed = 1
+const time = 0.001
 
-var posfx = 0
-var posfy = 0
-var posfz = 0
-var cod := Vector3(0, 2, 0)
+var posf := Vector3(0,0,0)
+var cod := Vector3(0,2,0)
+var sp := Vector3(0,0,0)
 
 signal playerpos(vx, vy, vz)
 
-func Velocity(init, final):
+func PlayerPos(init, final, axis):
 	if final > init:
+		if axis == "x": sp.x = speed
+		elif axis == "y": sp.y = speed
+		elif axis == "z": sp.z = speed
 		return init + speed
 	elif final < init:
+		if axis == "x": sp.x = -speed
+		elif axis == "y": sp.y = -speed
+		elif axis == "z": sp.z = -speed
 		return init - speed
 	else:
+		if axis == "x": sp.x = 0
+		elif axis == "y": sp.y = 0
+		elif axis == "z": sp.z = 0
 		return init
 	
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	var positions = parse_json(body.get_string_from_utf8())
 	for position in positions:
 		print(position)
-		posfx = position['X']
-		print(posfx)
-		posfy = position['Y']
-		print(posfy)
-		posfz = position['Z']
-		print(posfz)
-		while (cod.x != posfx or cod.y != posfy or cod.z != posfz):
-			cod.x = Velocity(cod.x, posfx)
-			cod.y = Velocity(cod.y, posfy)
-			cod.z = Velocity(cod.z, posfz)
+		posf.x = position['X']
+		print("X: ", posf.x)
+		posf.y = position['Y']
+		print("Y: ", posf.y)
+		posf.z = position['Z']
+		print("Z: ", posf.z)
+		while (cod.x != posf.x or cod.y != posf.y or cod.z != posf.z):
+			cod.x = PlayerPos(cod.x, posf.x, "x")
+			cod.y = PlayerPos(cod.y, posf.y, "y")
+			cod.z = PlayerPos(cod.z, posf.z, "z")
 			print(cod.x, ", ", cod.y, ", ", cod.z)
-			move_and_slide(cod)
+			move_and_slide(sp)
 			emit_signal("playerpos", cod.x, cod.y, cod.z)
-			yield(get_tree().create_timer(0.1), "timeout")
+			yield(get_tree().create_timer(time), "timeout")
